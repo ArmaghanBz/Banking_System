c231c36d6821e4c695b4987193018f0bb308cc5c
Armaghan Bin Zahid <80704109+ArmaghanBz@users.noreply.github.com>
2021-08-06 16:17:24 +0500

    Update-2
    in this, the track of your balance is kept during the transactions

--- Source.cpp
+++ Source.cpp
@@ -13,11 +13,12 @@ struct bank {
 };
 bank* head = NULL;
 void createNewAcc() {
+	system("cls");
 	int d = 0, acc_n; string na, t;
 	cout << "\t\tPlease enter new name: ";
 	cin.ignore();
 	getline(cin,na);
-	cout << "\t\tPlease enter new account: ";
+	cout << "\t\tPlease enter new account number: ";
 	cin >> acc_n;
 	cout << "\t\tPlease enter the type of the account: ";
 	cin.ignore();
@@ -33,9 +34,9 @@ void createNewAcc() {
 	newnode->balance = d;
 	//------------------------------------------------//
 	if (head == NULL) {
+		head = newnode;
 		newnode->next = NULL;
 		newnode->prev = NULL;
-		head = newnode;
 	}
 	else {
 		bank* temp = head;
@@ -47,22 +48,18 @@ void createNewAcc() {
 		newnode->next = NULL;
 	}
 }
-void transactions() {
-	string tempName;
-	cout << "\t\tPlease enter your name: ";
-	cin.ignore();
-	getline(cin, tempName);
+int transactions() {
+again:
+	system("cls");
 	int tempId;
-	cout << "\t\tPlease enter your Account Id: ";
+	cout << "\t\tPlease enter your Account number: ";
 	cin >> tempId;
 	bank* temp = head;
 	string recieverName, receiverAcc, recieverBankName, recieverAccountNo;
-	int sendingAmount;
+	int sendingAmount=0;
+	bool flag=false;
 	while (temp != NULL) {
-		if (temp->name != tempName && temp->account_no != tempId) {
-			cout << "\t\tThis account does not exits!!";
-		}
-		else {
+		if ( tempId == temp->account_no) {
 			system("cls");
 			cout << "\n\t\tAccount found!!\n\n";
 			cout << "\t\tPlease enter the name of the person to whom want to send the money: ";
@@ -70,39 +67,51 @@ void transactions() {
 			getline(cin, recieverName);
 			cout << "\t\tPlease enter the name of the bank of that person to whom want to send the money: ";
 			getline(cin, recieverBankName);
-			cout << "\t\tPlease enter aaccount number of that person to whom want to send the money: ";
+			cout << "\t\tPlease enter account number of that person to whom want to send the money: ";
 			getline(cin, recieverAccountNo);
 			cout << "\t\tPlease enter the amount: ";
 			cin >> sendingAmount;
-			temp->balance = temp->balance - sendingAmount; /*user balance will be updated after the
-			transaction*/
+			if (sendingAmount <= temp->balance) {
+				temp->balance = temp->balance - sendingAmount; /*user balance will be updated after the
+				transaction*/
+			}
+			else {
+				cout << "\n\t\tYour account doesn't have sufficient balance to perform transaction\n\n";
+				cout << "\t\tYour balance is: " << temp->balance << endl;
+				_getch();
+				return 1;
+			}
+			flag = true;
+			
 		}
 		temp = temp->next;
 	}
-	bank* temp1 = head;
-	while (temp1 != NULL) {
-		if (tempName != temp1->name && tempId != temp1->account_no) {
-			cout << "\t\tThis account does not exits!!";
-		}
-		else {
-			system("cls");
-			cout << "\n\t\tThe transaction slip: \n\n";
-			cout << "\t\tThe name: " << recieverName << endl;
-			cout << "\t\tThe account number: " << recieverAccountNo << endl;
-			cout << "\t\tThe reciever's bank: " << recieverBankName << endl;
-			cout << "\t\tThe Amount send: " << sendingAmount << endl;
-			temp1 = temp1->next;
-		}
+	if (flag==false) {
+		cout << "The account doesn't exits- Try again";
+		_getch();
+		goto again;
 	}
+	else {
+		system("cls");
+		cout << "\n\t\tThe transaction slip: \n\n";
+		cout << "\t\tThe name: " << recieverName << endl;
+		cout << "\t\tThe account number: " << recieverAccountNo << endl;
+		cout << "\t\tThe reciever's bank: " << recieverBankName << endl;
+		cout << "\t\tThe Amount send: " << sendingAmount << endl;
+	}
+
 }
 void display() {
+	system("cls");
 	bank* temp = head;
 	while (temp != NULL) {
-		cout<<"\t\tThe name: " << temp->name << endl;
+
+		cout<<"\n\n\t\tThe name: " << temp->name << endl;
 		cout <<"\t\tThe account number: " << temp->account_no << endl;
 		cout <<"\t\tThe deposited Amount on first time: " << temp->deposit << endl;
 		cout << "\t\tThe Balance: " << temp->balance << endl;
 		cout <<"\t\tThe type of the account: " << temp->type << endl;
+		cout << "\t\t---------------------------------------\n\n";
 		temp = temp->next;
 	}
 }
@@ -110,29 +119,43 @@ void display() {
 void main() {
 	//-----------------Main screen------------------//
 	mainwin:
-	cout << "\n\n\t\tTHE BANK ALHABIB" << endl << endl;
-	//cout << "\t\t Press enter to continue";
-	int temp=0;
+	cout << "\n\n\n\n\t\t\t\tThe World Bank" << endl << endl;
+	cout << "\t\t\t    Press enter to continue";
+	_getch();
+	system("cls");
+	int temp=0,temp1=0;
 	while (true) {
-		cout<<"\n\t\t---------------------------------------------\n";
+		menu:
 		cout << "\n\t\tPlease enter 1 to enter new account: \n";
 		cout << "\t\tPlease enter 2 to display the account: \n";
 		cout << "\t\tPlease enter 3 to transfering the amount : \n";
 		cout << "\t\tPlease enter 4 to exit the program : \n";
 		cin >> temp;
 		if (temp == 1) {
-			system("cls");
 			createNewAcc();
+			system("cls");
 		}
 		else if (temp == 2) {
-			system("cls");
 			display();
+			cout << "\n\t\tPress any key to continue: ";
+			_getch();
+			system("cls");
 		}
 		else if (temp == 3) {
-			system("cls");
-			transactions();
+			temp1=transactions();
+			if (temp1 == 1) {
+				system("cls");
+				goto menu;
+			}
+			else {
+				cout << "\n\t\tPress any key to continue: ";
+				_getch();
+				system("cls");
+			}
 		}
 		else if (temp == 4) {
+			system("cls");
+			cout << "\n\n\t\tProgram Exited!!\n\n";
 			exit(1);
 		}
 	}
